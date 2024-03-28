67e8acd30caac164e609b4b22b01a87591c6a9f1
Lotero123 <86239372+Lotero123@users.noreply.github.com>
2024-03-28 14:52:42 +0100

    Crear componentes y enrutamiento
    Creee los componentes de rutinas y ejercicios, se puede cambiar, y arregle los botones para cambiar de vistas

--- Proyecto-microservicio/src/app/app.routes.ts
+++ Proyecto-microservicio/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { EjerciciosComponent } from './ejercicios/ejercicios.component';
+import { RutinasComponent } from './rutinas/rutinas.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'rutinas', component: RutinasComponent},
+    {path: 'ejercicios', component: EjerciciosComponent},
+];
