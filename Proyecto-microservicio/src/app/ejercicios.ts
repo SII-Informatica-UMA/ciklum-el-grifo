9896306e52b5944fdd691ff96a06513b36c8772d
Juan Jose <juruizca@gmail.com>
2024-03-30 18:17:39 +0100

    Create ejercicios.ts

---
+++ Proyecto-microservicio/src/app/ejercicios.ts
@@ -0,0 +1,12 @@
+export interface Ejercicio {
+    nombre?: string;
+    descripcion?: string;
+    observaciones?: string;
+    tipo?: string;
+    musculosTrabajados?: string;
+    material?: string;
+    dificultad?: string;
+    multimedia?: string[];
+    id?: number;
+  }
+  
