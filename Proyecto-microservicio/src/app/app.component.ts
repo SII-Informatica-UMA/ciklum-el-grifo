43f1a24aac085e409e939f1ad03f52e350c31887
IsraelGomezUrbano <israelgomez2003@uma.es>
2024-03-30 16:09:42 +0100

    Instalación de bootstrap

--- Proyecto-microservicio/src/app/app.component.ts
+++ Proyecto-microservicio/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
