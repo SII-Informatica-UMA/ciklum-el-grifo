6f77c769365106c33aa30c866386faff9353f664
Juan Jose <juruizca@gmail.com>
2024-04-24 20:03:35 +0200

    FUNCIONA
    ARREGLAO

--- jpa/spring-jpa/src/main/java/grifo/spring/jpa/demo/repositories/EjercicioRepository.java
+++ jpa/spring-jpa/src/main/java/grifo/spring/jpa/demo/repositories/EjercicioRepository.java
@@ -3,6 +3,8 @@ package grifo.spring.jpa.demo.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import grifo.spring.jpa.demo.entities.Ejercicio;
 
@@ -10,6 +12,6 @@ public interface EjercicioRepository  extends JpaRepository<Ejercicio,Long>{
     List<Ejercicio> findByIdEntrenador(Long idEntrenador);
     List<Ejercicio> findByNombre(String nombre);
     
-    //@Query(select e from Ejercicio e where e.dificultad = :dificultad)
-   //List<Ejercicio> miConsultaCompleja(@Param("dificultad") String dificultad);
+   @Query("select e from Ejercicio e where e.dificultad = :dificultad")
+   List<Ejercicio> miConsultaCompleja(@Param("dificultad") String dificultad);
 }
--- jpa/spring-jpa/src/main/java/grifo/spring/jpa/demo/repositories/RutinaRepository.java
+++ jpa/spring-jpa/src/main/java/grifo/spring/jpa/demo/repositories/RutinaRepository.java
@@ -3,13 +3,15 @@ package grifo.spring.jpa.demo.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import grifo.spring.jpa.demo.entities.Rutina;
 
 public interface RutinaRepository extends JpaRepository<Rutina,Long> {
     List<Rutina> findByIdEntrenador(Long idEntrenador);
     List<Rutina> findByIdAndNombreOrderByNombreAsc(Long id, String nombre);
-   // @Query("SELECT CASE WHEN COUNT(r) > 0 THEN TRUE ELSE FALSE END FROM Rutina r WHERE EXISTS (SELECT 1 FROM r.ejercicios f WHERE f.ejercicio.id = :idEjercicio)")
-   // boolean existsRutinaWithEjercicio(@Param("idEjercicio") Long idEjercicio);
+    @Query("SELECT CASE WHEN COUNT(r) > 0 THEN TRUE ELSE FALSE END FROM Rutina r WHERE EXISTS (SELECT 1 FROM r.ejercicios f WHERE f.ejercicios.id = :idEjercicio)")
+    boolean existsRutinaWithEjercicio(@Param("idEjercicio") Long idEjercicio);
     
 }
