a5433a49add7711e37e8a6adc0204fe8ce5e0d29
Juan Jose <juruizca@gmail.com>
2024-05-25 12:02:12 +0200

    Tests
    Implementados la gran mayoría de tests con la base de datos vacia

--- jpa/spring-jpa/src/main/java/grifo/spring/jpa/demo/security/SecurityConfguration.java
+++ jpa/spring-jpa/src/main/java/grifo/spring/jpa/demo/security/SecurityConfguration.java
@@ -1,6 +1,7 @@
 package grifo.spring.jpa.demo.security;
 
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,7 +15,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfguration {
    @Autowired
-
    public SecurityConfguration() {
    }
 
--- jpa/spring-jpa/src/test/java/grifo/spring/jpa/demo/EjerciciosRutinasAplicationTests.java
+++ jpa/spring-jpa/src/test/java/grifo/spring/jpa/demo/EjerciciosRutinasAplicationTests.java
@@ -1,5 +1,10 @@
 package grifo.spring.jpa.demo;
 
+import static org.assertj.core.api.Assertions.*;
+
+import java.net.URI;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -9,25 +14,20 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import org.springframework.web.util.UriBuilder;
 import org.springframework.web.util.UriBuilderFactory;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import grifo.spring.jpa.demo.dtos.EjercicioDTO;
+import grifo.spring.jpa.demo.dtos.RutinaDTO;
 import grifo.spring.jpa.demo.entities.Ejercicio;
+import grifo.spring.jpa.demo.entities.Rutina;
 import grifo.spring.jpa.demo.repositories.EjercicioRepository;
 import grifo.spring.jpa.demo.repositories.RutinaRepository;
-
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) 
 @DisplayName("En el servicio de Rutinas y Ejercicios")
@@ -65,7 +65,7 @@ public class EjerciciosRutinasAplicationTests {
     }
 
     private RequestEntity<Void> get(String scheme, String host, int port, String path) {
-        URI uri = uri(scheme, host, port, path);
+        URI uri = UriComponentsBuilder.newInstance().scheme(scheme).host(host).port(port).path(path).queryParam("entrenador",1).build().toUri();
         var peticion = RequestEntity.get(uri)
             .accept(MediaType.APPLICATION_JSON)
             .build();
@@ -79,7 +79,7 @@ public class EjerciciosRutinasAplicationTests {
         return peticion;
     }
 
-    private <T> RequestEntity<T> post(String scheme, String host, int port, String path, T object) {
+    private <T> RequestEntity<T> post(String scheme, String host, int port,  T object,String path) {
         URI uri = uri(scheme, host, port, path);
         var peticion = RequestEntity.post(uri)
             .contentType(MediaType.APPLICATION_JSON)
@@ -87,7 +87,7 @@ public class EjerciciosRutinasAplicationTests {
         return peticion;
     }
 
-    private <T> RequestEntity<T> put(String scheme, String host, int port, String path, T object) {
+    private <T> RequestEntity<T> put(String scheme, String host, int port,  T object,String path) {
         URI uri = uri(scheme, host, port, path);
         var peticion = RequestEntity.put(uri)
             .contentType(MediaType.APPLICATION_JSON)
@@ -95,26 +95,141 @@ public class EjerciciosRutinasAplicationTests {
         return peticion;
     }
 
-    @Nested
+      @Nested
     @DisplayName("cuando no hay ejercicios")
     public class EjerciciosVacios {
-        @Test
-        @DisplayName("error por ejercicio inexistente")
-        public void devuelveErrorEjercicioInexistente() {
 
-            var peticion = get("http", "localhost",port, "/ejercicio/1");
+        @Test
+        @DisplayName("devuelve la lista de ejercicios vacía")
+        public void devuelveEjercicios() {
+            var peticion = get("http", "localhost", port, "/ejercicios");
 
             var respuesta = restTemplate.exchange(peticion,
-					new ParameterizedTypeReference<Ejercicio>() {});
-            
+                    new ParameterizedTypeReference<List<EjercicioDTO>>() {
+                    });
+
+            assertThat(respuesta.getStatusCode().value()).isEqualTo(200);
+            assertThat(respuesta.getBody()).isEmpty();
+        }
+
+        @Test
+        @DisplayName("error al intentar obtener ejercicio especifico cuando no hay ejercicios")
+        public void obtenerEjercicioNoExiste() {
+            Long idEjercicio = 1L;
+
+            var peticion = get("http", "localhost", port, "/ejercicios/" + idEjercicio);
+            var respuesta = restTemplate.exchange(peticion, Ejercicio.class);
+
             assertThat(respuesta.getStatusCode().value()).isEqualTo(404);
         }
+
+        @Test
+        @DisplayName("Error al actualizar un ejercicio especifico no existente")
+        public void actualizarEjercicioNoExiste() {
+            Long idEjercicio = 1L;
+            EjercicioDTO ejercicioActualizado = new EjercicioDTO();
+            ejercicioActualizado.setNombre("Nuevo Ejercicio 1");
+
+            var peticion = put("http", "localhost", port, ejercicioActualizado, "/ejercicios/" + idEjercicio);
+
+            var respuesta = restTemplate.exchange(peticion, Ejercicio.class);
+
+            assertThat(respuesta.getStatusCode().value()).isEqualTo(404);
+        }
+
+        @Test
+        @DisplayName("error al eliminar ejercicio especifico no existente")
+        public void eliminarEjercicioNoExiste() {
+            Long idEjercicio = 1L;
+
+            var peticion = delete("http", "localhost", port, "/ejercicios/" + idEjercicio);
+            var respuesta = restTemplate.exchange(peticion, Void.class);
+
+            assertThat(respuesta.getStatusCode().value()).isEqualTo(404);
+        }
+
+        @Test
+        @DisplayName("Pone un ejercicio correctamente con la base de datos vacia")
+        public void ponerEjercicioNoExiste() {
+            Long idEjercicio = 1L;
+            EjercicioDTO nuevoEjercicio = new EjercicioDTO();
+            nuevoEjercicio.setNombre("Nuevo Ejercicio 1");
+
+            var peticion = post("http", "localhost", port, nuevoEjercicio,"/ejercicios/" + idEjercicio);
+            var respuesta = restTemplate.exchange(peticion, Ejercicio.class);
+
+            assertThat(respuesta.getStatusCode().value()).isEqualTo(200);
+        }
+
+
     }
 
     @Nested
     @DisplayName("cuando no hay rutinas")
     public class RutinasVacias {
 
+        @Test
+        @DisplayName("devuelve la lista de rutinas vacía")
+        public void devuelveEjercicios() {
+            var peticion = get("http", "localhost", port, "/rutinas");
+
+            var respuesta = restTemplate.exchange(peticion,
+                    new ParameterizedTypeReference<List<RutinaDTO>>() {
+                    });
+
+            assertThat(respuesta.getStatusCode().value()).isEqualTo(200);
+            assertThat(respuesta.getBody()).isEmpty();
+        }
+
+        @Test
+        @DisplayName("error al intentar obtener rutina especifica cuando no hay rutinas")
+        public void obtenerRutinaNoExiste() {
+            Long idRutina = 1L;
+
+            var peticion = get("http", "localhost", port, "/rutinas/" + idRutina);
+            var respuesta = restTemplate.exchange(peticion, Rutina.class);
+
+            assertThat(respuesta.getStatusCode().value()).isEqualTo(404);
+        }
     }
+
+    @Test
+    @DisplayName("Error al actualizar una rutina especifica no existente")
+    public void actualizarRutinaNoExiste() {
+        Long idEjercicio = 1L;
+        RutinaDTO rutinaActualizada = new RutinaDTO();
+        rutinaActualizada.setNombre("Nuevo Ejercicio 1");
+
+        var peticion = put("http", "localhost", port, rutinaActualizada, "/ejercicios/" + idEjercicio);
+
+        var respuesta = restTemplate.exchange(peticion, Rutina.class);
+
+        assertThat(respuesta.getStatusCode().value()).isEqualTo(404);
+    }
+
+    
+    @Test
+    @DisplayName("error al eliminar rutina especifica no existente")
+    public void eliminarRutinaNoExiste() {
+        Long idRutina = 1L;
+
+        var peticion = delete("http", "localhost", port, "/rutinas/" + idRutina);
+        var respuesta = restTemplate.exchange(peticion, Void.class);
+
+        assertThat(respuesta.getStatusCode().value()).isEqualTo(404);
+    }
+
+    @Test
+        @DisplayName("Pone una rutina correctamente con la base de datos vacia")
+        public void ponerRutinaNoExiste() {
+            Long idRutina = 1L;
+            RutinaDTO nuevaRutina = new RutinaDTO();
+            nuevaRutina.setNombre("Nuevo Ejercicio 1");
+
+            var peticion = post("http", "localhost", port, nuevaRutina,"/rutinas/" + idRutina);
+            var respuesta = restTemplate.exchange(peticion, Rutina.class);
+
+            assertThat(respuesta.getStatusCode().value()).isEqualTo(200);
+        }
 
 }
